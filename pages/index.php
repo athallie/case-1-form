fdaeb9313ec75ef9e74cdeccb8dae85e39c828fa
eavelie <athallahnaufalr@gmail.com>
2024-03-16 21:50:38 +0700

    modified templates

--- pages/index.php
+++ pages/index.php
@@ -1,31 +1,32 @@
-<?php 
-  include_once '../includes/header.php';
-?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-    <form action="output.php" method="post">
-        <div class="input-form">
-            <input type="number" name="var1" id="var1" required>
-        </div>
-        <div class="input-form">
-            <input type="number" name="var2" id="var2" required>
-        </div>
-        <div class="input-form">
-            <input type="number" name="var3" id="var3" required>
-        </div>
-        <div class="input-form">
-            <input type="submit" value="Hitung">
-        </div>
-    </form>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
 <?php
-    include_once '../includes/footer.php';
+include_once '../includes/header.php';
+?>
+    <!doctype html>
+    <body class="bg bg-success">
+    <div id="index-cont" class="container-fluid">
+        <div class="row bg bg-dark">
+            <div id="left-column" class="col bg bg-black">
+                <form action="output.php" method="post">
+                    <div class="input-form">
+                        <input type="number" name="var1" id="var1" required>
+                    </div>
+                    <div class="input-form">
+                        <input type="number" name="var2" id="var2" required>
+                    </div>
+                    <div class="input-form">
+                        <input type="number" name="var3" id="var3" required>
+                    </div>
+                    <div class="input-form">
+                        <input type="submit" value="Hitung">
+                    </div>
+                </form>
+            </div>
+            <div id="right-column" class="col bg-danger">
+                Tes
+            </div>
+        </div>
+    </div>
+    </body>
+    </html>
+<?php
+include_once '../includes/footer.php';
