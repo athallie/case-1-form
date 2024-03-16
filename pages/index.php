7983265d69760bbec9dbf5e9e4a8391292609d4a
eavelie <athallahnaufalr@gmail.com>
2024-03-16 20:49:38 +0700

    modified template

--- pages/index.php
+++ pages/index.php
@@ -1,6 +1,5 @@
 <?php 
-  include_once 'header.php';
-  include_once 'footer.php';
+  include_once '../includes/header.php';
 ?>
 <!doctype html>
 <html lang="en">
@@ -11,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <form action="kalkulator.php" method="post">
+    <form action="output.php" method="post">
         <div class="input-form">
             <input type="number" name="var1" id="var1" required>
         </div>
@@ -28,3 +27,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
+<?php
+    include_once '../includes/footer.php';
