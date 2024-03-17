c0d7f740aac4dc9206cee3ae228c823a15915201
eavelie <athallahnaufalr@gmail.com>
2024-03-17 14:12:29 +0700

    modified structures and styles

--- index.php
+++ index.php
@@ -2,30 +2,60 @@
 include_once 'includes/header.php';
 ?>
     <!doctype html>
-    <body class="bg bg-success">
-    <div id="index-cont" class="container-fluid">
-        <div class="row bg bg-dark">
-            <div id="left-column" class="col bg bg-black">
+    <body>
+    <div id="index-outer-container" class="container-fluid">
+        <div class="row row-outer">
+            <div id="left-column" class="col pt-5">
                 <form id="variable-form" action="pages/output.php" method="post">
                     <div class="input-group">
                         <span class="input-group-text" id="unit-r">Rp.</span>
-                        <input class="form-control" type="number" id="var-r" step="any" min="0" placeholder="21000" required>
+                        <input class="form-control form-control-lg" type="number" id="var-r" step="any" min="0"
+                               placeholder="21000" required>
                     </div>
                     <div class="input-group">
-                        <input class="form-control" type="number" id="var-i" step="any" min="0" placeholder="i" oninput="validateDivisor()" required>
+                        <input class="form-control form-control-lg" type="number" id="var-i" step="any" min="0"
+                               placeholder="i" oninput="validateDivisor()" required>
                         <span class="input-group-text" id="unit-i">%</span>
                     </div>
                     <div class="input-group">
-                        <input class="form-control" type="number" id="var-n" step="any" min="0" placeholder="n" required>
-                        <span class="input-group-text" id="unit-n">tahun</span>
+                        <input class="form-control form-control-lg" type="number" id="var-n" step="any" min="0"
+                               placeholder="n" required>
+                        <span class="input-group-text" id="unit-n">pembayaran</span>
                     </div>
-                    <div class="input-group">
-                        <input type="submit" value="Hitung" onclick="validateDivisor()">
+                    <div id="submit-button" class="input-group form-control-lg">
+                        <input type="submit" value="Hitung" class="btn btn-lg" onclick="validateDivisor()">
                     </div>
                 </form>
             </div>
-            <div id="right-column" class="col bg-danger">
-                Tes
+            <div id="right-column" class="col">
+                <div id="index-inner-right-container" class="container-fluid">
+                    <div class="row row-inner mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4>Future Value of Ordinary Annuity</h4>
+                                <p>
+                                    Future Value of Ordinary Annuity (FVOA) adalah jumlah total uang pada tanggal tertentu di
+                                    masa depan. Jumlah total uang tersebut mempertimbangkan serangkaian pembayaran sama yang
+                                    dilakukan di
+                                    akhir setiap periode beserta dengan bunga yang diperoleh. Pada dasarnya, FVOA menunjukkan
+                                    besaran nilai investasi di masa depan.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row row-inner">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4>Variabel</h4>
+                                <p>
+                                    R = Pembayaran/deposit yang dilakukan di setiap period <br>
+                                    i = Bunga per periode <br>
+                                    n = Jumlah total pembayaran <br>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -33,6 +63,7 @@ include_once 'includes/header.php';
         document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('variable-form').reset();
         });
+
         function validateDivisor() {
             let i = document.getElementById('var-i');
             if (parseFloat(i.value) === 0) {
